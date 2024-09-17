0a93a76402d4e492bf20cdc2e86bcf2a1e723d03
Efe Belli <efebelli17@gmail.com>
2024-09-17 16:43:11 +0300

    Fix animation speed and runner speed balance

--- Assets/Scripts/RunnerController.cs
+++ Assets/Scripts/RunnerController.cs
@@ -10,12 +10,18 @@ public class RunnerController : MonoBehaviour
     private InputActionMap map;
     [SerializeField] Rigidbody2D rb;
     private bool _isHoldingRunKey;
-    private float _currentSpeed=1;
-    
+    private float _currentSpeed;
+    private float _startingSpeed = 10;
+    private Animator animator;
+    private float _thresholdSpeed = 20;
+    private float _maxSpeed = 35;
+    private float _maxAnimSpeed = 6;
     private void Awake()
     {
         map = new InputActionMap("playerControls");
         run = map.AddAction("runAction", binding: "<Keyboard>/space");
+        animator = GetComponent<Animator>();
+        _currentSpeed = _startingSpeed;
     }
     // Start is called before the first frame update
     void Start()
@@ -28,8 +34,33 @@ public class RunnerController : MonoBehaviour
     {
         if (_isHoldingRunKey)
         {
-            _currentSpeed += Time.deltaTime;
+            if (_currentSpeed < _maxSpeed)
+            {
+                if (_currentSpeed < _thresholdSpeed)
+                {
+                    _currentSpeed += Time.deltaTime * 2;
+                    if (animator.speed < _maxAnimSpeed)
+                    {
+                        animator.speed += _currentSpeed *2* _maxAnimSpeed/_maxSpeed * Time.deltaTime/11;
+                    }
+                }
+                else if (_currentSpeed >= _thresholdSpeed)
+                {
+                    _currentSpeed += Time.deltaTime;
+                    
+                    if (animator.speed < _maxAnimSpeed)
+                    {
+                        animator.speed += _currentSpeed * _maxAnimSpeed / _maxSpeed * Time.deltaTime/11;
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("max speed has been achieved");
+            }
+            
         }
+        Debug.Log($"current speed => {_currentSpeed}\n animator speed => {animator.speed}");
     }
     private void OnEnable()
     {
@@ -59,7 +90,7 @@ public class RunnerController : MonoBehaviour
         if (_isHoldingRunKey)
         {
             Debug.Log("move runner clled");
-            rb.MovePosition(rb.position + (Vector2.right * _currentSpeed*Time.deltaTime));
+            rb.MovePosition(rb.position + (Vector2.right * _currentSpeed * Time.deltaTime * 3));
         }
     }
 
